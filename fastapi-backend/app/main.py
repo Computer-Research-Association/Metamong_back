03b75d1658c9deea9fb565e027e955f977fd9574
Jangmyun <cndlsrb2739@gmail.com>
2026-01-10 23:25:04 +0900

    [Chore]: FastAPI app 객체 title 지정

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from app.db.database import engine
 from app.db import models
 
+from app.core.config import settings
+
 # TODO: 서버 본격 배포 시 Alembic 도입 필요
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title=settings.PROJECT_NAME)
 
 
 @app.get("/")
