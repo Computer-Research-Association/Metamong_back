3e90adcec280eb9c2a5ffe96f2e32dbe76500fb3
Jangmyun <cndlsrb2739@gmail.com>
2026-01-11 19:52:07 +0900

    [Feat]: sqlalchemy.url 을 환경변수에서 동적으로 주입

--- fastapi-backend/migrations/env.py
+++ fastapi-backend/migrations/env.py
@@ -5,10 +5,14 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 from app.db import models
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+if settings.DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
