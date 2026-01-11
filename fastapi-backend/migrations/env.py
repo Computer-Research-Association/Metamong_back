6cf9bfecb9e44b0cbba596246f5593c1396b4644
Jangmyun <cndlsrb2739@gmail.com>
2026-01-11 19:48:57 +0900

    [Feat]: target metadata를 기존에 models Base 의 메타데이터로 설정

--- fastapi-backend/migrations/env.py
+++ fastapi-backend/migrations/env.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
+from app.db import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
