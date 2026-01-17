a2a01d90a4087e98c52901cba54ba17253b33274
Jangmyun <cndlsrb2739@gmail.com>
2026-01-17 17:31:44 +0900

    [Chore]: Alembic 도입했으므로 모델 create_all 제거

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -6,9 +6,6 @@ from sqlalchemy.orm import Session
 
 from app.core.config import settings
 
-# TODO: 서버 본격 배포 시 Alembic 도입 필요
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title=settings.PROJECT_NAME)
 
 
