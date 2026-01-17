a038f7ad4c16fcbfcdbcba46c1b5509ba83224bf
Jangmyun <cndlsrb2739@gmail.com>
2026-01-18 02:22:27 +0900

    [Feat]: login 로직 처리 (email 조회하고 user가 없으면 생성, 기본 RC=토레이)

--- fastapi-backend/app/services/auth_service.py
+++ fastapi-backend/app/services/auth_service.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import Session
 from typing import Dict, Any
+from datetime import datetime, timezone
+
+from app.db.models import User
+from app.db.enums import AuthProvider, RC, UserStatus
+from app.core.security import create_access_token
 
 
 class AuthService:
@@ -18,3 +23,31 @@ class AuthService:
         # TODO: 다른 프로바이더 (NAVER, KAKAO) 도 균일한 정보를 리턴하도록 분기처리
 
         return {}
+
+    async def login(self, provider: str, user_data: Dict) -> str:
+
+        email = user_data.get("email")
+        if not email:
+            raise ValueError("Email not found.")
+
+        user = self.db.query(User).filter(User.email == email).first()
+
+        if not user:
+            user = User(
+                email=email,
+                nickname=user_data.get("nickname"),
+                real_name=user_data.get("name"),
+                auth_provider=AuthProvider[provider.upper()],
+                rc=RC.Torrey,
+                status=UserStatus.ACTIVE
+            )
+            self.db.add(user)
+        else:
+            # TODO : 런타임에는 정상적으로 동작하지만, Column[datetime] 타입과의 불일치로 인한 문제 해결 (Pylance)
+            # last_login_at 의 타입힌트
+            user.last_login_at = datetime.now(timezone.utc)  # type: ignore
+
+        self.db.commit()
+        self.db.refresh(user)
+
+        return create_access_token(subject=user.id)
