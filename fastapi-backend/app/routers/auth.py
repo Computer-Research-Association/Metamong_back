f1102c8dc5814eede4055c8fc61be896919aeb8b
Jangmyun <cndlsrb2739@gmail.com>
2026-01-18 02:44:42 +0900

    [Feat]: 여러 provider를 지원하는 login router 작성

--- fastapi-backend/app/routers/auth.py
+++ fastapi-backend/app/routers/auth.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+
+from app.core.oauth import oauth
+from app.core.config import settings
 
 router = APIRouter()
+
+
+@router.get("/login/{provider}")
+async def login(provider: str, request: Request):
+    if provider not in ["google", "naver", "kakao"]:
+        raise HTTPException(status_code=404, detail="Invalid provider")
+
+    client = oauth.create_client(provider)
+    redirect_uri = f"{settings.BACKEND_URL}/api/auth/callback/{provider}"
+    return await client.authorize_redirect(request, redirect_uri)
