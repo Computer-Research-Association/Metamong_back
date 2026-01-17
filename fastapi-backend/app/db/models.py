d01d7d197e28817aeb9bd10f868b11807e713219
Jangmyun <cndlsrb2739@gmail.com>
2026-01-17 19:17:39 +0900

    [Feat]: Room model ERD 따라서 created

--- fastapi-backend/app/db/models.py
+++ fastapi-backend/app/db/models.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, BigInteger, String, DateTime, Integer, Enum, ForeignKey, Index
+from sqlalchemy import Column, BigInteger, String, DateTime, Integer, Enum, ForeignKey, Index, Boolean
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.db.database import Base
 
-from app.db.enums import AuthProvider, RC, UserStatus
+from app.db.enums import AuthProvider, RC, UserStatus, RoomType, OwnerType
 
 
 class User(Base):
@@ -48,3 +48,25 @@ class Team(Base):
 
 
 Index("idx_teams_owner_user_id", Team.owner_user_id)
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    room_type = Column(Enum(RoomType), nullable=False)
+    name = Column(String(50), nullable=False)
+
+    owner_type = Column(Enum(OwnerType), nullable=False)
+    owner_id = Column(BigInteger)
+
+    is_public = Column(Boolean, nullable=False, default=False)
+    password_hash = Column(String(255))
+
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+
+    created_at = Column(DateTime(timezone=True),
+                        nullable=False, server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False,
+                        server_default=func.now(), onupdate=func.now())
