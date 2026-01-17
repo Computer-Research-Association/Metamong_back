2c224aed2e5d9abc025002933821c9e79a8abdf6
Jangmyun <cndlsrb2739@gmail.com>
2026-01-18 00:06:31 +0900

    [Feat]: 만료기간 따로 설정할 수 있도록 함. 아니면 default 24 hours

--- fastapi-backend/app/core/security.py
+++ fastapi-backend/app/core/security.py
@@ -5,3 +5,8 @@ from app.core.config import settings
 
 
 def create_access_token(subject: Any, expires_delta: timedelta | None = None) -> str:
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + \
+            timedelta(hours=settings.JWT_EXPIRATION_HOURS)
