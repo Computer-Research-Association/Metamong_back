b5d1b48bc389a2be72f41450452a69a7b0746d9b
SeoGyeongmi <22200360@handong.ac.kr>
2026-01-22 14:06:21 +0900

    [FEAT] : add enum mbti

--- fastapi-backend/app/db/enums.py
+++ fastapi-backend/app/db/enums.py
@@ -47,3 +47,22 @@ class FriendStatus(Enum):
     PENDING = "PENDING"
     ACCEPTED = "ACCEPTED"
     BLOCKED = "BLOCKED"
+
+
+class MBTI(Enum):
+    ISTJ = "ISTJ"
+    ISFJ = "ISFJ"
+    INFJ = "INFJ"
+    INTJ = "INTJ"
+    ISTP = "ISTP"
+    ISFP = "ISFP"
+    INFP = "INFP"
+    INTP = "INTP"
+    ESTP = "ESTP"
+    ESFP = "ESFP"
+    ENFP = "ENFP"
+    ENTP = "ENTP"
+    ESTJ = "ESTJ"
+    ESFJ = "ESFJ"
+    ENFJ = "ENFJ"
+    ENTJ = "ENTJ"
