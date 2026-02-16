2a015591236fc6acaa3b0679083b65600da3eac0
SeoGyeongmi <22200360@handong.ac.kr>
2026-02-16 18:51:57 +0900

    [Feat]: Player state 유저 위치 스키마 추가

--- colyseus-server/src/rooms/schema/MyRoomState.ts
+++ colyseus-server/src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,6 @@
-import { Schema, type } from "@colyseus/schema";
+import { Schema, type, MapSchema } from "@colyseus/schema";
+import { PlayerState } from "./PlayerState";
 
 export class MyRoomState extends Schema {
-
-  @type("string") mySynchronizedProperty: string = "Hello world";
-
+  @type({ map: PlayerState }) players = new MapSchema<PlayerState>();
 }
