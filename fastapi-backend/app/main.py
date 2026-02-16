d7371ae9b5d72aaa2a1a9b362e33f6e2112ec604
Jangmyun <cndlsrb2739@gmail.com>
2026-02-16 23:59:09 +0900

    [Feat]: 로컬 테스트를 위한 cors url 추가

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -11,7 +11,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         settings.FRONTEND_URL,
+        "http://localhost:8000",
         "http://localhost:3000",
+        "http://127.0.0.1:8000",
         "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
